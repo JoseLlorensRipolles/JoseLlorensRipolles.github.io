04cc0afae093807d850f5ebef05795fb57e0547c
Jose Manuel Llorens <jose.llorens.ripolles@gmail.com>
2021-01-15 18:48:39 +0100

    Added down arrow for scroll suggestion

--- src/app/components/image-header/image-header.component.ts
+++ src/app/components/image-header/image-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-image-header',
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./image-header.component.scss']
 })
 export class ImageHeaderComponent implements OnInit {
+  scrolled = 0;
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll($event) {
+    const numb = window.scrollY;
+    if (numb >= 50) {
+      this.scrolled = 1;
+    }
+    else {
+      this.scrolled = 0;
+    }
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
